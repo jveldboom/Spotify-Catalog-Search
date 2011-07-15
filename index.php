5d78affd9918755004844bad52f39df1aa33f10c
John Veldboom <jveldboom@gmail.com>
2011-07-15 00:50:36 -0400

    added google analytics and social buttons forced side bar to be fixed width to keep it out the way

--- index.php
+++ index.php
@@ -53,6 +53,19 @@
 	}
 	</script>
 </head>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-543394-25']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <body>
 <style>
 body {
@@ -62,6 +75,7 @@ body {
 		font-size: 12px;
 	}
 a {color: #afe059;}
+a:hover {color: #60910f;}
 th {
 text-align: left;
 font-size: 12px;
@@ -113,21 +127,31 @@ color: #afe059;
 #extra {display: none; font-size: 10px;}
 
 .side_bar {
-position: absolute;
-right: 10;
-top: 10;
+	position: absolute;
+	width: 300px;
+	overflow: auto;
+	right: 10;
+	top: 10;
 }
 
 
 </style>
 <div class="side_bar">
-	<img src="http://upload.wikimedia.org/wikipedia/commons/archive/7/79/20110126015610!Spotify_logo.png">
+	<img src="http://upload.wikimedia.org/wikipedia/commons/archive/7/79/20110126015610!Spotify_logo.png" style="float: right; margin: 0 0 5px 5px;">
 	<h2>Browse Catalog</h2>
 	This was developed to help your browse the Spotify catalog before you <a href="http://www.spotify.com/us/get-spotify/overview/">purchase the paid packages</a> - which BTW is well worth it!
 	<br /><br />
-	<a href="#">Visit our github</a> page to help improve or if you find any bugs.
+	<a href="https://github.com/jveldboom/Spotify-Catalog-Brower">Visit our github</a> page to help us improve or squash bugs.
 	<br /><br />
 	<a href="http://open.spotify.com/user/jveldboom"><img src="https://www.spotify.com/static/images/social-badge-en_US.png" alt="Follow me on Spotify" title="Follow me on Spotify"></a>
+	<br /><br />
+	<a href="http://twitter.com/#!/jveldboom" title="twitter"><img src="http://cdn1.iconfinder.com/data/icons/vector_social_media_icons/16px/twitter.png" border=0></a>
+	<a href="http://www.facebook.com/johnveldboom" title="Facebook"><img src="http://cdn1.iconfinder.com/data/icons/vector_social_media_icons/16px/facebook.png" border=0></a>
+	<a href="https://plus.google.com/117818393527494042939/about" title="Google+"><img src="https://lh3.googleusercontent.com/-5IAsU14e0rA/Th_DOWo7rjI/AAAAAAAADRU/0SVKF1I5xoo/s16/g-plus-icon-16x16.png" border=0></a>
+	<br /><br />
+	<iframe src="http://www.facebook.com/plugins/like.php?app_id=122324204524908&amp;href=http%3A%2F%2Fwww.linein.org%2Fexamples%2Fspotify%2F&amp;send=false&amp;layout=button_count&amp;width=250&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font=verdana&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:250px; height:21px;" allowTransparency="true"></iframe>
+	<br /><br />
+	<g:plusone size="medium"></g:plusone>
 </div>
 <form onsubmit="spotify_search(); return false;">
 <select id="type">
@@ -140,5 +164,6 @@ top: 10;
 </form>
 <br /><br />
 <div id="results"></div>
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
 <body>
 </html>
